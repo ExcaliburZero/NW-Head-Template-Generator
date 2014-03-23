dbcd6c9145845c46ac195fc39456b674b9212830
rocketslime11 <cwellsny@nycap.rr.com>
2014-03-23 11:04:43 -0400

    added checks for article type and article class

--- Nintendo Wiki Head Template Generator.py
+++ Nintendo Wiki Head Template Generator.py
@@ -13,19 +13,30 @@ which = which.lower()
 print("If one or more of the following categories do not apply to the article, or if the answer to them is no, then simply press enter to skip them.")
 
 #Simple Inputs
-type = input("Article type?")
+article_type = input("Article type?")
+
+#Check article type
+if not (article_type == "" or article_type == "Character" or article_type == "Location" or article_type == "Item" or article_type == "Person" or article_type == "System" or article_type == "Accessory" or article_type == "Game" or article_type == "Species" or article_type == "Organization" or article_type == "Company" or article_type == "Product" or article_type == "Soundtrack" or article_type == "Fan Website" or article_type == "Attack" or article_type == "Group" or article_type == "Event" or article_type == "Year" or article_type == "Terminology" or article_type == "Magazine" or article_type == "Class" or article_type == "Series" or article_type == "Chapter" or article_type == "Ability" or article_type == "Other"):
+    print ("Incorrect article type! A list of the correct article types can be found at http://nintendo.wikia.com/wiki/Template:Head#Types \n Remeber that you must input it with correct spelling and the first letter of each word capitalized.")
+    article_type = input("Article type?")
+    
 quote = input("Quote?")
 speaker = input("Speaker of Quote?")
 stub = input("Stub? (If not, leave blank)")
 article_class = input("Article class? (A-E, X)")
+
+#Check article class
+if not (article_type == "" or article_type == "A" or article_type == "B" or article_type == "C" or article_type == "D" or article_type == "E" or article_type == "X" or article_type == "?"):
+    print ("Incorrect article class! A list of the correct article classes can be found at http://nintendo.wikia.com/wiki/Nintendo_Wiki:Page_Classification \n Remeber that you must input it as a capital letter.")
+    article_class = input("Article class? (A-E, X)")
 
 #Output
 if which == "simple":
     
     #Output simple head template
     print("{{Head")
-    if type != "":
-        print("| type = " + type)
+    if article_type != "":
+        print("| type = " + article_type)
     if quote != "":
         print("| quote = " + quote)
     if speaker != "":
@@ -50,8 +61,8 @@ elif which == "complex":
     
     #Output complex head template
     print("{{Head")
-    if type != "":
-        print("| type = " + type)
+    if article_type != "":
+        print("| type = " + article_type)
     if quote != "":
         print("| quote = " + quote)
     if speaker != "":
