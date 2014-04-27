f83580529006ba30bd3252c80ba9e0deac03c937
rocketslime11 <cwellsny@nycap.rr.com>
2014-04-27 15:28:37 -0400

    fixed names of GUI and TUI versions
    switched names of TUI and GUI version since they were mixed up

--- Nintendo Wiki Head Template Generator (TUI).py
+++ Nintendo Wiki Head Template Generator (TUI).py
@@ -6,129 +6,92 @@ Purpose: Generates code for head templates for Nintendo Wiki articles
 Programer: ExcaliburZero / Rocketslime_1_1
 
 """
+##Check if a basic or complex head template is wanted
+which = input("Would you like a simple or complex head template?")
+which = which.lower()
 
-#Imports
-from tkinter import *
+#Check template version
+while which != "simple" and which != "complex":
+    print("----------\nIncorrect head template type.")
+    which = input("Would you like a simple or complex head template?")
+    which = which.lower()
 
-#Generate Head Template Function
-def generate_head():
+#Print notice about entering variables
+print("----------\nIf one or more of the following categories do not apply to the article, or if the answer to them is no, then simply press enter to skip them.\n----------")
 
-    #Grab SIMPLE variables
-    narticle_type = article_type.get()
-    nquote = quote.get()
-    nspeaker = speaker.get()
-    nstub = stub.get()
-    narticle_class = article_class.get()
+#Simple Inputs
+article_type = input("Article type?")
 
-    #Grab COMPLEX variables
-    nfeatured = featured.get()
-    nprotected = protected.get()
-    nunreleased = unreleased.get()
-    nfor1 = for1.get()
-    nfor2 = for2.get()
-    nfor3 = for3.get()
+#Check article type
+while not (article_type == "" or article_type == "Character" or article_type == "Location" or article_type == "Item" or article_type == "Person" or article_type == "System" or article_type == "Accessory" or article_type == "Game" or article_type == "Species" or article_type == "Organization" or article_type == "Company" or article_type == "Product" or article_type == "Soundtrack" or article_type == "Fan Website" or article_type == "Attack" or article_type == "Group" or article_type == "Event" or article_type == "Year" or article_type == "Terminology" or article_type == "Magazine" or article_type == "Class" or article_type == "Series" or article_type == "Chapter" or article_type == "Ability" or article_type == "Other"):
+    print ("----------\nIncorrect article type! A list of the correct article types can be found at http://nintendo.wikia.com/wiki/Template:Head#Types \n Remeber that you must input it with correct spelling and the first letter of each word capitalized.")
+    article_type = input("Article type?")
 
-    #Put together output
-    output = "{|"
-    if not narticle_type == "":
-        output = output + "\n| type = " + narticle_type
-    if not nquote == "":
-        output = output + "\n| quote = \"" + nquote + "\""
-    if not nspeaker == "":
-        output = output + "\n| speaker = " + nspeaker
-    if not nstub == "":
-        output = output + "\n| stub = " + "x"
-    if not narticle_class == "":
-        output = output + "\n| class = " + narticle_class
-    if not nfeatured == "":
-        output = output + "\n| featured = x"
-    if not nprotected == "":
-        output = output + "\n| protected = x"
-    if not nunreleased == "":
-        output = output + "\n| unreleased = x"
-    if not nfor1 == "":
-        output = output + "\n| for = x"
-        output = output + "\n| for1 = " + nfor1
-    if not nfor2 == "":
-        output = output + "\n| for2 = " + nfor2
-    if not nfor3 == "":
-        output = output + "\n| for3 = " + nfor3
-    output = output + "\n|}"
+#Continue Simple Inputs    
+quote = input("Quote?")
+speaker = input("Speaker of Quote?")
+stub = input("Stub? (If not, leave blank)")
+article_class = input("Article class? (A-E, X)")
 
-    #Output template code
-    output_template = Text(theGUI, height = 16, width = 23)
-    output_template.grid(row = 14, column = 1)
-    output_template.insert(END, output)
-    #output_template.config(state = DISABLED)
-    #output_template = Label(theGUI, text = output).grid(row = 7, column = 1)
+#Check article class
+while not (article_class == "" or article_class == "A" or article_class == "B" or article_class == "C" or article_class == "D" or article_class == "E" or article_class == "X" or article_class == "?"):
+    print ("----------\nIncorrect article class! A list of the correct article classes can be found at http://nintendo.wikia.com/wiki/Nintendo_Wiki:Page_Classification \n Remeber that you must input it as a capital letter.")
+    article_class = input("Article class? (A-E, X)")
 
-    return
+#Output
+if which == "simple":
+    
+    #Output simple head template
+    print("----------\n{{Head")
+    if article_type != "":
+        print("| type = " + article_type)
+    if quote != "":
+        print("| quote = " + quote)
+    if speaker != "":
+        print("| speaker = " + speaker)
+    if stub != "":
+        print("| stub = x")
+    if article_class != "":
+        print("| class = " + article_class)
+    print("}}")
+    
+elif which == "complex":
 
-#Generate GUI
-theGUI = Tk()
-theGUI.geometry("400x550+100+200")
-theGUI.title("Head Template Generator")
-
-#Setup SIMPLE variables
-article_type = StringVar()
-quote = StringVar()
-speaker = StringVar()
-stub = StringVar()
-article_class = StringVar()
-
-#Setup SIMPLE labels
-label_intro = Label(theGUI, text="Welcome to the Head Template Generator").grid(row = 0, columnspan = 2)
-label_article_type = Label(theGUI, text="type").grid(row = 1, column = 0)
-label_quote = Label(theGUI, text="quote").grid(row = 2, column = 0)
-label_speaker = Label(theGUI, text="speaker").grid(row = 3, column = 0)
-label_stub = Label(theGUI, text="stub").grid(row = 4, column = 0)
-label_article_class = Label(theGUI, text="class").grid(row = 5, column = 0)
-
-#Setup SIMPLE entry boxes
-article_type = Entry(theGUI, textvariable = article_type)
-article_type.grid(row = 1, column = 1)
-quote = Entry(theGUI, textvariable = quote)
-quote.grid(row = 2, column = 1)
-speaker = Entry(theGUI, textvariable = speaker)
-speaker.grid(row = 3, column = 1)
-stub = Entry(theGUI, textvariable = stub)
-stub.grid(row = 4, column = 1)
-article_class = Entry(theGUI, textvariable = article_class)
-article_class.grid(row = 5, column = 1)
-
-#Setup COMPLEX variables
-featured = StringVar()
-protected = StringVar()
-unreleased = StringVar()
-for1 = StringVar()
-for2 = StringVar()
-for3 = StringVar()
-
-#Setup COMPLEX labels
-label_blank = Label(theGUI, text="").grid(row = 6, column = 0)
-label_featured = Label(theGUI, text="featured").grid(row = 7, column = 0)
-label_protected = Label(theGUI, text="protected").grid(row = 8, column = 0)
-label_unreleased = Label(theGUI, text="unreleased").grid(row = 9, column = 0)
-label_for1 = Label(theGUI, text="for1").grid(row = 10, column = 0)
-label_for2 = Label(theGUI, text="for2").grid(row = 11, column = 0)
-label_for3 = Label(theGUI, text="for3").grid(row = 12, column = 0)
-
-#Setup COMPLEX entry boxes
-featured = Entry(theGUI, textvariable = featured)
-featured.grid(row = 7, column = 1)
-protected = Entry(theGUI, textvariable = protected)
-protected.grid(row = 8, column = 1)
-unreleased = Entry(theGUI, textvariable = unreleased)
-unreleased.grid(row = 9, column = 1)
-for1 = Entry(theGUI, textvariable = for1)
-for1.grid(row = 10, column = 1)
-for2 = Entry(theGUI, textvariable = for2)
-for2.grid(row = 11, column = 1)
-for3 = Entry(theGUI, textvariable = for3)
-for3.grid(row = 12, column = 1)
-
-#Setup button
-button_generate = Button(theGUI, text = "Generate", command = generate_head).grid(row = 13, column = 1)
-
-#Add GUI to main loop
-theGUI.mainloop()
+    #Complex template variables
+    featured = input("Is this a featured article?")
+    protected = input("Is this article protected?")
+    unreleased = input("Is this article about an unreleased game?")
+    hfor = input("Does this article require a for section?")
+    if hfor != "":
+        for1 = input("for1?")
+        for2 = input("for2")
+        for3 = input("for3?")
+    
+    #Output complex head template
+    print("----------\n{{Head")
+    if article_type != "":
+        print("| type = " + article_type)
+    if quote != "":
+        print("| quote = " + quote)
+    if speaker != "":
+        print("| speaker = " + speaker)
+    if stub != "":
+        print("| stub = x")
+        
+    if featured != "":
+        print("| featured = x")
+    if protected != "":
+        print("| protected = x")
+    if article_class != "":
+        print("| class = " + article_class)
+    if unreleased != "":
+        print("| unreleased = x")
+    if hfor != "":
+        print("| for = x")
+        if for1 != "":
+            print("| for1 = " + for1)
+        if for2 != "":
+            print("| for2 = " + for2)
+        if for3 != "":
+            print("| for3 = " + for3)    
+    print("}}")
